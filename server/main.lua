b4a1e86c5953ca855706980bf418c043c5a7f774
dufuspaelli <me@dimits.net>
2022-02-21 14:26:09 +0200

    make sure despawned cars are not stored

--- server/main.lua
+++ server/main.lua
@@ -8,7 +8,29 @@ RegisterNetEvent("setSiren", function(siren, carNetId)
     sirens[carNetId].siren = siren
 end)
 
-QBCore.Functions.CreateCallback('qb-cnr:server:listCopCars', function(source, cb, safe)
+QBCore.Functions.CreateCallback('qb-simplesirens:server:listSirenCars', function(source, cb, safe)
    -- print(sirens)
     cb(sirens)
+end)
+
+AddEventHandler("entityRemoved", function(handle)
+    local entityModel = GetEntityModel(handle)
+    local carModel = GetHashKey("police3")
+    local entity = NetworkGetNetworkIdFromEntity(handle)
+
+    if entityModel == carModel then 
+        sirens[entity] = nil
+    end
+
+end)
+
+CreateThread(function()
+
+    while true do 
+        for val,key in pairs(cops) do 
+            print(val)
+        end
+        Wait(1000) 
+    end
+
 end)
