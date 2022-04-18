5d01f8ffbb46187dac81b7cf8b9b5a080eac0aea
dufuspaelli <me@dimits.net>
2022-04-19 02:43:39 +0300

    properly ReleaseSoundId so the script doesnt bug out

--- client/main.lua
+++ client/main.lua
@@ -6,44 +6,27 @@ local sirenNormal = "VEHICLES_HORNS_SIREN_1"
 local sirenAlt = "VEHICLES_HORNS_SIREN_2"
 local sirenWarn = "VEHICLES_HORNS_POLICE_WARNING"
 local sirenList = {}
-
 RegisterCommand("sirenDefault", function()
-    if isSirenChange(sirenNormal) or not toggle then
-        toggle = true
+    if isSirenChange(sirenNormal) then
         playSiren(sirenNormal)
-    elseif toggle and not isSirenChange(sirenNormal) then
-        toggle = false
-        playSiren("muted")
-    end
+    end 
 end)
 
 RegisterCommand("sirenAlt", function()
-    if isSirenChange(sirenAlt) or not toggle then 
-        toggle = true
+    if isSirenChange(sirenAlt) then 
         playSiren(sirenAlt)
-    elseif toggle and not isSirenChange(sirenAlt) then 
-        toggle = false
-        playSiren("muted")
     end
 end)
 
 RegisterCommand("sirenWarn", function()
-    if isSirenChange(sirenWarn) or not toggle then 
-        toggle = true
+    if isSirenChange(sirenWarn) then 
         playSiren(sirenWarn)
-    elseif toggle and not isSirenChange(sirenWarn) then 
-        toggle = false
-        playSiren("muted")
     end
 end)
 
 RegisterCommand("sirenHorn", function()
-    if isSirenChange(sirenHorn) or not toggle then 
-        toggle = true
+    if isSirenChange(sirenHorn) then 
         playSiren(sirenHorn)
-    elseif toggle and not isSirenChange(sirenHorn) then 
-        toggle = false
-        playSiren("muted")
     end
 end)
 
@@ -55,7 +38,7 @@ RegisterCommand("sirenMute", function()
     if isSirenChange("muted") or Entity(playerCar).state.currentSiren == nil then 
         playSiren("muted")
     else
-        local siren = sirenNormal
+        siren = sirenNormal
         playSiren(siren)
     end
 end)
@@ -72,7 +55,7 @@ CreateThread(function()
         QBCore.Functions.TriggerCallback('qb-simplesirens:server:listSirenCars', function(sirenServerList)
            sirenList = sirenServerList or sirenList
         end)
-        Wait(5000)
+        Wait(1000)
     end
 end)
 
@@ -83,14 +66,16 @@ CreateThread(function()
                 local car = NetToVeh(carNet)
                 SetVehicleHasMutedSirens(car, true)
                 local muted = val.siren == "muted" or not IsVehicleSirenOn(car) or IsVehicleSeatFree(car, -1) or false
-                local currentSiren = Entity(car).state.currentSiren
+                local currentSiren = Entity(car).state.currentSiren or "muted"
                 local sirenChange = currentSiren ~= val.siren or false
-                local sirenOn = Entity(car).state.sirenOn == nil or Entity(car).state.sirenOn == false and IsVehicleSirenOn(car) or false
+                local sirenOn = Entity(car).state.sirenOn == nil and IsVehicleSirenOn(car) or false
                 if muted then 
                     StopSound(Entity(car).state.soundId)
+                    ReleaseSoundId(Entity(car).state.soundId)
                     Entity(car).state:set('sirenOn', false, true)
                     Entity(car).state:set("currentSiren", val.siren ,true)
                 elseif sirenOn then 
+                    ReleaseSoundId(Entity(car).state.soundId)
                     local soundId = GetSoundId()
                     Entity(car).state:set('sirenOn', true, true)
                     Entity(car).state:set('soundId', soundId, true)
@@ -99,6 +84,7 @@ CreateThread(function()
                 elseif sirenChange then 
                     local soundId = GetSoundId()
                     StopSound(Entity(car).state.soundId)
+                    ReleaseSoundId(Entity(car).state.soundId)
                     Entity(car).state:set('sirenOn', true, true)
                     Entity(car).state:set('soundId', soundId, true)
                     Entity(car).state:set("currentSiren", val.siren ,true)
@@ -113,7 +99,6 @@ end)
 function playSiren(siren)
     local playerPed = PlayerPedId()
     local playerCar = GetVehiclePedIsIn(playerPed,false)
-    local soundId = GetSoundId()
     local carNetId = VehToNet(playerCar)
     if GetVehicleClass(playerCar) == 18 and IsVehicleSirenOn(playerCar) then 
         TriggerServerEvent("setSiren", siren, carNetId)
